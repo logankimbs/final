58a7b21a8884056c29d515a39ed873fe7831e729
Logan Kimball <logankimbs@gmail.com>
2021-04-15 11:25:03 -0600

    Created back-end

---
+++ back-end/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// connection to database
+mongoose.connect('mongodb://localhost:27017/final-project', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+app.use(cookieParser());
+
+app.use(cookieSession({
+    name: 'session',
+    keys: [
+        'secretValue'
+    ],
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000
+    }
+}));
+
+app.listen(3002, () => console.log('Server listening on port 3002'));
