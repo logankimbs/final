1e3942638b685c74a6315804121a8556df16f7a6
Logan Kimball <logankimbs@gmail.com>
2021-04-15 17:08:04 -0600

    Updating

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,9 +1,7 @@
 <template>
     <div class="app">
         <div class="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/login">Login</router-link> |
-            <router-link to="/signup">Create New Account</router-link>
+            <router-link to="/">Home</router-link>
         </div>
         <router-view />
     </div>
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import Signup from '../views/Signup.vue'
 
 Vue.use(VueRouter)
 
@@ -10,14 +8,6 @@ const routes = [{
     path: '/',
     name: 'Home',
     component: Home
-}, {
-    path: '/login',
-    name: 'Login',
-    component: Login
-}, {
-    path: '/signup',
-    name: 'Signup',
-    component: Signup
 }]
 
 const router = new VueRouter({
