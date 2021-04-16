f6bf21cabb30271ecffd2e9088e0a4e62ca5ebbe
Logan Kimball <logankimbs@gmail.com>
2021-04-16 09:17:30 -0600

    Styled add button

--- front-end/src/components/Dashboard.vue
+++ front-end/src/components/Dashboard.vue
@@ -1,16 +1,19 @@
 <template>
     <div class="dashboard">
         <Navbar />
+        <Add />
     </div>
 </template>
 
 <script>
 import Navbar from '@/components/Navbar.vue';
+import Add from '@/components/Add.vue';
 
 export default {
     name: 'Dashboard',
     components: {
-        Navbar
+        Navbar,
+        Add
     }
 }
 </script>
