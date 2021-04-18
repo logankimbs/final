309d079294983aa72675238db40e20d8d7f0086d
Logan Kimball <logankimbs@gmail.com>
2021-04-18 11:04:04 -0600

    Updating

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,13 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import BootstrapVue from "bootstrap-vue"
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "bootstrap-vue/dist/bootstrap-vue.css"
 
 Vue.config.productionTip = false
-
-Vue.use(BootstrapVue)
 
 let data = {  user: null  }
 
