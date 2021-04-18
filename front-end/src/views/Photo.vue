03e2a009f88b516bd56f010bbbfb8d507739f15e
Logan Kimball <logankimbs@gmail.com>
2021-04-18 09:57:35 -0600

    Added content to photo page

--- front-end/src/views/Photo.vue
+++ front-end/src/views/Photo.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="photo">
+        <h1>{{this.photo.title}}</h1>
+        <h3>{{this.photo.user.username}}</h3>
+        <p>{{this.formatDate(this.photo.created)}}</p>
         <img :src="photo.path" class="img-fluid" alt="Responsive image">
+        <p>{{this.photo.description}}</p>
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import moment from 'moment';
 
 export default {
     name: 'Photo',
@@ -35,6 +40,14 @@ export default {
                 let response = await axios.get('/api/users');
                 this.$root.$data.user = response.data.user;
             } catch (error) {  this.$root.$data.user = null;  }
+        },
+
+        formatDate(date) {
+            if (moment(date).diff(Date.now(), 'days') < 15) {
+                return moment(date).fromNow();
+            } else {
+                return moment(date).format('d MMMM YYYY');
+            }
         }
     }
 }
